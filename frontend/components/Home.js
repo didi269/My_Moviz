1e229ef4fb67ad0c8492de797c9d2da9d7aa60ac
idriss <idriss.adinani@gmai.com>
2023-11-15 17:50:31 +0100

    fhghf

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://my-moviz-back-psi.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
 
